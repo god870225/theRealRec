134f51a8c9c02b86ebdfe9124f40c96fe295cb86
god870225 <god870225@naver.com>
2016-10-05 09:49:38 +0900

    최초커밋

---
+++ theRealErec/www/code/common.js
@@ -0,0 +1,19 @@
+$(function () {
+	var $activate_scrollator4 = $('#activate_scrollator4');
+
+
+	$activate_scrollator4.bind('click', function () {
+		var $document_body = $('body');
+		if ($document_body.data('scrollator') === undefined) {
+			$document_body.scrollator({
+				custom_class: 'body_scroller'
+			});
+			$activate_scrollator4.val('destroy scrollator')
+		} else {
+			$document_body.scrollator('destroy');
+			$activate_scrollator4.val('activate scrollator')
+		}
+	});
+
+	$activate_scrollator4.trigger('click');
+});
