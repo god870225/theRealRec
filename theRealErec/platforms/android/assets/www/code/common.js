cd80f98404282723b3ef083931d328fee77eb29a
god870225 <god870225@naver.com>
2016-10-06 15:49:38 +0900

    a

--- theRealErec/platforms/android/assets/www/code/common.js
+++ theRealErec/platforms/android/assets/www/code/common.js
@@ -134,7 +134,7 @@ return true;
 //개발IP셋팅
 function commonIp(){
 	var commonIp = "";
-	var dev = true;
+	var dev = false;
 	if(dev){
 		commonIp = "http://192.168.0.39:8080";
 		//commonIp = "117.52.97.40";
